dde83434ca5bd30ea27b1935709ff93e7c8971ee
Daniel Thompson <daniel@redfelineninja.org.uk>
2024-03-13 17:22:57 +0000

    lava-rebuild-alpine: a test definition that regenerates the rootfs

---
+++ lava-rebuild-alpine.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#
+# lava-rebuild-apline.sh
+#
+# Self-hosted image regeneration!
+#
+
+set -ex
+
+# Install prerequisites
+apk add --no-interactive --no-progress e2fsprogs zstd
+
+# Rebuild the rootfs
+./alpine-make-rootfs alpine-base-latest.tar --script-chroot bootable/tweak-image
+zstd -19 --force alpine-base-latest.tar
+
+# Package the rootfs as an ext4 filesystem
+mkfs.ext4 -L rootfs alpine-base-latest.ext4 1G
+mkdir sysimage
+mount -o loop alpine-base-latest.ext4 sysimage
+tar -C sysimage/ -xf alpine-base-latest.tar
+
+# Package the rootfs as an initramfs
+(cd sysimage/; find . -print | cpio -o -H newc) | zstd -19 > alpine-base-latest.cpio.zst
+
+# Finalize and compress the ext4 image
+umount sysimage
+zstd -19 --rm --force alpine-base-latest.ext4
